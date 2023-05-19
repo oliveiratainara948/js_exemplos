f3ae8beba8ca74cb666ea256800e8bb3a62fd607
Taii <126676872+oliveiratainara948@users.noreply.github.com>
2023-05-19 13:52:06 -0300

    JS

---
+++ script.js
@@ -0,0 +1,9 @@
+var email = prompt(" Digite seu e-mail","");
+alert("O e-mail " + email + " foi coletado com sucesso.");
+
+// var valor = confirm("Vamos juntos aprender a programar em JS?")
+// if(valor == true){
+//     alert("Parabéns, vamos lá!!");
+// } else{
+//     alert(" Opcão errada, aperte F5 para recarregar a página.");
+// }
